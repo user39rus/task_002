770c04b6c18ee74799053581268a157ae11fdbd3
turnipan <915520@mail.ru>
2024-03-01 19:19:09 +0300

    pascal_triangel

---
+++ src/pascal_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    if (scanf("%d", &n) == 1 && n > 0 && n < 30) {
+        printf("1");
+        --n;
+        int c = n;
+        for (int k = 1; k <= n; k++) {
+            printf(" %d", c);
+            c = c * (n - k);
+            c = c / (k + 1);
+        }
+    } else
+        printf("Puck you, Verter!");
+    return 0;
+}
